8f62fb4611adc87540cd0deca2de2a40b5330c07
TooFlyRyGuy <ryanrobertlee@gmail.com>
2025-12-12 11:59:34 -0800

    Updated AdminDashboard.tsx

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -974,7 +974,7 @@ const AdminDashboard: React.FC<AdminProps> = ({ onLogout, onLaunchKiosk, user })
               <p className="text-slate-600 mt-2">Connect your accounts to unlock powerful features</p>
             </header>
 
-            <Settings tenant={tenant} onSave={handleSaveSettings} isAdmin={true} />
+            <Settings tenant={tenant} onSave={handleSaveSettings} isAdmin={userProfile?.subscription_tier === 'ADMIN'} />
           </div>
         )}
 
