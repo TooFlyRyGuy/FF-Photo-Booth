b0796fb2fa05c7edd7c4c0b320a0cc737713449c
TooFlyRyGuy <ryanrobertlee@gmail.com>
2025-12-27 17:32:04 -0800

    Updated PngToJpgMigration.tsx

--- components/PngToJpgMigration.tsx
+++ components/PngToJpgMigration.tsx
@@ -37,6 +37,7 @@ export function PngToJpgMigration() {
           headers: {
             'Authorization': `Bearer ${session.access_token}`,
             'Content-Type': 'application/json',
+            'apikey': import.meta.env.VITE_SUPABASE_ANON_KEY,
           },
         }
       );
