269cfb82c1d63fc3b46a72ec55022f1107c1a4a3
TooFlyRyGuy <ryanrobertlee@gmail.com>
2026-02-01 14:11:45 -0800

    Updated 20260201215701_remote_schema.sql

--- supabase/migrations/20260201215701_remote_schema.sql
+++ supabase/migrations/20260201215701_remote_schema.sql
@@ -1284,7 +1284,7 @@ $$;
 
 ALTER FUNCTION "public"."handle_new_user"() OWNER TO "postgres";
 
-DROP TRIGGER IF EXISTS "on_auth_user_created" ON auth.users;
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
 
 
